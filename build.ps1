5f2b8ba3ae515b513de30c2846d0b81192541c4b
Edmund Dipple <elmundio1987@gmail.com>
2023-02-24 13:22:23 +0000

    fix missing brackets in bookmarklet wrapper

--- build.ps1
+++ build.ps1
@@ -5,6 +5,6 @@ $function = Get-Content main.js
 $minified = [string]::join("", $function.split("`n"))
 $minified = $minified -replace "    ",""
 $minified = $minified -replace "  ",""
-$minified = "javascript:void function(){$minified};"
+$minified = "javascript:void function(){$minified}();"
 
 $minified | Out-File -FilePath ".\release.js"
